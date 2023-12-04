c002a2fb58d9e53848ffb15a650e97739aef4644
George <george.stratulat10@gmail.com>
2023-12-04 10:28:18 +0200

    added response model and network layer

---
+++ RBChallenge/Network/NetworkRequests/Default.swift
@@ -0,0 +1,24 @@
+//
+//  Default.swift
+//  RBChallenge
+//
+//  Created by George Stratulat on 03.12.2023.
+//
+
+import Foundation
+import Alamofire
+
+struct DefaultRequest: NetworkRequest {
+    typealias responseModel = UsersResponse
+    var path = "/api/"
+    let page: Int
+    let results: Int
+    let seed = "abc"
+    var parameters: Parameters? {
+        [
+            "page": page,
+            "results": results,
+            "seed": seed
+        ]
+    }
+}
